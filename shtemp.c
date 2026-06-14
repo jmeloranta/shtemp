928b182cbb357e180efcd8daf2a53e855a48fdef
Jussi Eloranta <eloranta@aa6kj.hopto.org>
2026-06-13 23:22:16 -0500

    Added graphing for temp & humidity.

--- shtemp.c
+++ shtemp.c
@@ -10,15 +10,100 @@
 #include <unistd.h>
 #include <termios.h>
 #include <gtk/gtk.h>
+#include <cairo/cairo.h>
 
 #define DEVICE "/dev/serial/by-id/usb-Adafruit_SHT4x_Trinkey_M0_C2CD0A4F503059384B2E3120FF0E230D-if00"
 
-// Update every 5 sec (5000 ms)
-#define UPDATE 5000
+#define NPTS 128
+#define WIDTH 200
+#define HEIGHT 200
 
-int fd;
-double cur_temp = -1.0, cur_hum = -1.0;
-GtkWidget *temp, *hum;
+#define MIN_TEMP -20.0
+#define MAX_TEMP 40.0
+#define TEMP_STEP 5.0
+#define MIN_HUM 0.0
+#define MAX_HUM 100.0
+#define HUM_STEP 10.0
+
+// Update every 2 sec (2000 ms)
+#define UPDATE 2000
+
+int fd, n_data = 0;
+double cur_temp = -1.0, cur_hum = -1.0, temps[NPTS], hums[NPTS];
+GtkWidget *temp, *hum, *draw_temp, *draw_hum;
+cairo_surface_t *surface_temp, *surface_hum;
+cairo_t *cairo_temp, *cairo_hum;
+
+void convert_data(double *orig, double *new_x, double *new_y, double min, double max) {
+
+  int i;
+
+  for (i = 0; i < n_data; i++) {
+    new_x[i] = ((double) WIDTH) * ((double) i) / (double) NPTS;
+    new_y[i] = ((double) HEIGHT) * (1.0 - ((orig[i] - min) / (max - min)));
+  }
+}
+
+void func_draw(GtkDrawingArea *area, cairo_t *cairo, int width, int height, gpointer user_data) {
+
+  int i, j;
+  double data_x[NPTS], data_y[NPTS], *data;
+  char buf[32];
+
+  cairo_set_source_rgb(cairo, 1.0, 1.0, 1.0);
+  cairo_paint(cairo);
+
+  cairo_set_source_rgb(cairo, 0.0, 0.0, 0.0);
+  cairo_set_line_width(cairo, 1.0);
+
+  if(user_data == (void *) temps) 
+    i = ((double) HEIGHT) * TEMP_STEP / (MAX_TEMP - MIN_TEMP);
+  else
+    i = ((double) HEIGHT) * HUM_STEP / (MAX_HUM - MIN_HUM);
+  for(j = 0; j <= height; j += i) {
+    cairo_move_to(cairo, 0, j);
+    cairo_line_to(cairo, width, j);
+  }
+  cairo_stroke(cairo);
+  
+  cairo_select_font_face(cairo, "serif", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_BOLD);
+  cairo_set_font_size(cairo, 14.0);
+  cairo_set_source_rgb(cairo, 0.0, 0.0, 1.0);
+  if(user_data == (void *) temps) {
+    cairo_move_to(cairo, 5.0, 11.0);
+    sprintf(buf, "%.1f", MAX_TEMP);
+    cairo_show_text(cairo, buf);
+    cairo_move_to(cairo, 5.0, HEIGHT - 3.0);
+    sprintf(buf, "%.1f", MIN_TEMP);
+    cairo_show_text(cairo, buf);
+  } else {
+    cairo_move_to(cairo, 5.0, 11.0);
+    sprintf(buf, "%.1f", MAX_HUM);
+    cairo_show_text(cairo, buf);
+    cairo_move_to(cairo, 5.0, HEIGHT - 3.0);
+    sprintf(buf, "%.1f", MIN_HUM);
+    cairo_show_text(cairo, buf);
+    cairo_move_to(cairo, WIDTH-25.0, HEIGHT-3.0);
+    sprintf(buf, "%.1f", (UPDATE / 1000.0) * NPTS);
+    cairo_show_text(cairo, buf);
+  }
+
+  cairo_set_source_rgb(cairo, 1.0, 0.0, 0.0);
+  cairo_set_line_width(cairo, 2.0);
+  cairo_set_line_join(cairo, CAIRO_LINE_JOIN_ROUND);
+
+  if(n_data > 0) {
+    if(user_data == (void *) temps)
+      convert_data(temps, data_x, data_y, MIN_TEMP, MAX_TEMP);
+    else
+      convert_data(hums, data_x, data_y, MIN_HUM, MAX_HUM);
+    cairo_move_to(cairo, data_x[0], data_y[0]);
+    for(i = 1; i < n_data; i++)
+      cairo_line_to(cairo, data_x[i], data_y[i]);
+    cairo_stroke_preserve(cairo);    
+  }
+  gtk_widget_queue_draw(GTK_WIDGET(area));
+}
 
 int read_untilcr(char *buf, int maxlen) {
   
@@ -85,10 +170,27 @@ static gboolean update_data(void *asd) {
   } while (buf[0] == '#');
   sscanf(buf, "%*d, %le, %le, %*d", &cur_temp, &cur_hum);  
 
+  if(n_data < NPTS) {
+    temps[n_data] = cur_temp;
+    hums[n_data] = cur_hum;
+    n_data++;
+  } else {
+    int i;
+    for (i = 0; i < NPTS-1; i++) {
+      temps[i] = temps[i+1];
+      hums[i] = hums[i+1];
+    }
+    temps[NPTS-1] = cur_temp;
+    hums[NPTS-1] = cur_hum;
+  }
+
   sprintf(buf, "<span color='blue'>%2.1lf C</span>", cur_temp);
   gtk_label_set_markup(GTK_LABEL(temp), buf);
   sprintf(buf, "<span color='blue'>%2.2lf %%</span>", cur_hum);
   gtk_label_set_markup(GTK_LABEL(hum), buf);
+
+  func_draw(GTK_DRAWING_AREA(draw_temp), cairo_temp, 200, 200, (void *) temps);
+  func_draw(GTK_DRAWING_AREA(draw_hum), cairo_hum, 200, 200, (void *) hums);
 
   return 1;
 }
@@ -101,7 +203,7 @@ int main(int argc, char *argv[]) {
   gtk_init();
   window = gtk_window_new();
   gtk_window_set_title(GTK_WINDOW(window), "Temperature & Humidty");
-  gtk_window_set_default_size(GTK_WINDOW(window), 150, 40);
+  gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, 50 + 2 * HEIGHT);
 
   temp_label = gtk_label_new("Temperature:");
   gtk_label_set_xalign(GTK_LABEL(temp_label), 0.0); // 0 = left, 1 = right
@@ -112,14 +214,25 @@ int main(int argc, char *argv[]) {
   temp = gtk_label_new("-----");
   gtk_widget_set_hexpand(GTK_WIDGET(temp), TRUE);
   gtk_label_set_xalign(GTK_LABEL(temp), 0.5);
+  draw_temp = gtk_drawing_area_new();  
+  gtk_widget_set_size_request(draw_temp, WIDTH, HEIGHT);
+  gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(draw_temp), func_draw, (void *) temps, NULL);
+  surface_temp = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+  cairo_temp = cairo_create(surface_temp);
 
   hum = gtk_label_new("-----");
   gtk_widget_set_hexpand(GTK_WIDGET(hum), TRUE);
   gtk_label_set_xalign(GTK_LABEL(hum), 0.5);
+  draw_hum = gtk_drawing_area_new();  
+  gtk_widget_set_size_request(draw_hum, WIDTH, HEIGHT);
+  gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(draw_hum), func_draw, (void *) hums, NULL);
+
+  surface_hum = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, WIDTH, HEIGHT);
+  cairo_hum = cairo_create(surface_hum);
 
   hbox1 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
   hbox2 = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 2);
-    vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 2);
+  vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 4);
 
   gtk_box_append(GTK_BOX(hbox1), temp_label);
   gtk_box_append(GTK_BOX(hbox1), temp);
@@ -129,6 +242,8 @@ int main(int argc, char *argv[]) {
 
   gtk_box_append(GTK_BOX(vbox), hbox1);
   gtk_box_append(GTK_BOX(vbox), hbox2);
+  gtk_box_append(GTK_BOX(vbox), draw_temp);
+  gtk_box_append(GTK_BOX(vbox), draw_hum);
 
   gtk_window_set_child(GTK_WINDOW(window), vbox);
 
@@ -146,5 +261,10 @@ int main(int argc, char *argv[]) {
     g_main_context_iteration(NULL, TRUE);
 
   close(fd);
+  cairo_destroy(cairo_temp);
+  cairo_destroy(cairo_hum);
+  cairo_surface_destroy(surface_temp);
+  cairo_surface_destroy(surface_hum);
+
   return 0;
 }
